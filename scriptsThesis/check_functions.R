0e725700642a1b7d80fe9c0f3b121f1472aeeb60
AndreaE <farfanqbb@gmail.com>
2025-12-02 11:45:23 +0100

    license cc0-1.0 added to check functions

--- scriptsThesis/check_functions.R
+++ scriptsThesis/check_functions.R
@@ -1,3 +1,10 @@
+#<!--
+# SPDX-FileCopyrightText: 2025 Helmholtz-Zentrum hereon GmbH
+# SPDX-License-Identifier: CC0-1.0
+# SPDX-FileContributor Andrea Farfan <farfanqbb@gmail.de>
+#  -->
+
+
 library(dplyr)
 library(lubridate)
 library(BrownShrimp)
